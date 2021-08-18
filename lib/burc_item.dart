6a947100bbbcf1d27157fbf22e8ded887ea64101
mehmet ali <mehmetalier83@hotmail.com>
2021-08-18 10:01:05 +0300

    first commit

---
+++ lib/burc_item.dart
@@ -0,0 +1,48 @@
+import 'package:burc_projesi/model/burc_detay.dart';
+import 'package:flutter/material.dart';
+
+import 'model/burc.dart';
+
+
+class BurcItem extends StatelessWidget {
+  final Burc listelenenBurc;
+  const BurcItem({required this.listelenenBurc, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var myTextStyle = Theme.of(context).textTheme;
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Card(
+        elevation: 4,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => BurcDetay(secilenBurc: listelenenBurc),
+                ),
+              );
+            },
+            leading: Image.asset(
+              "images/images" + listelenenBurc.burcKucukResim,
+            ),
+            title: Text(
+              listelenenBurc.burcAdi,
+              style: myTextStyle.headline5,
+            ),
+            subtitle: Text(
+              listelenenBurc.burcTarih,
+              style: myTextStyle.subtitle1,
+            ),
+            trailing: Icon(
+              Icons.arrow_forward_ios,
+              color: Colors.pink,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
